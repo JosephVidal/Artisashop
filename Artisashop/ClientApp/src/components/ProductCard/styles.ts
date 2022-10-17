e64610c04666ccc889cfc33e6c11b92d1839bde0
Joseph <joseph.vidal@epitech.eu>
2022-10-17 16:44:39 +0200

    [Frontend][Structure]: Add ProductCard component

---
+++ Artisashop/ClientApp/src/components/ProductCard/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ProductCardWrapper = styled.div`
+    #product-card {
+        display: flex;
+        flex-direction: column;
+        border-radius: var(--elem-radius);
+        padding: 20px;
+        color: inherit;
+        text-decoration: inherit;
+        transition: 0.3s;
+
+        img {
+            width: 400px;
+            height: 400px;
+        }
+
+        p {
+            font-family: Helvetica;
+            margin: 5px;
+        }
+
+        :hover {color: var(--artshp-orange);}
+    }
+
+    #product-name, #product-price {font-size: 1.5em;}
+
+    #product-serie {font-weight: lighter}
+`;
