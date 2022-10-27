001897e597a8e400ba70c903ddd6b8334af6a9ab
Joseph <joseph.vidal@epitech.eu>
2022-10-27 12:31:29 +0200

    [Frontent][MOD]: Using right types for inputs in login and register pages

--- Artisashop/ClientApp/src/pages/Login/index.tsx
+++ Artisashop/ClientApp/src/pages/Login/index.tsx
@@ -7,8 +7,8 @@ const Login = () => (
       <h1>Artisashop</h1>
       <h2>Connexion</h2>
       <form action="submit">
-        <input className="credentials" type="text" placeholder="Email"/>
-        <input className="credentials" type="text" placeholder="Mot de passe"/>
+        <input className="credentials" type="email" placeholder="Email"/>
+        <input className="credentials" type="password" placeholder="Mot de passe"/>
         <button type="submit" id="register-button" className="red-button">Se connecter</button>
       </form>
       <p>Pas encore inscrit? <a href="/register">Rejoignez-nous</a> !</p>
