36b2ca60ea509d83b08d481351d8f5bdadbd5bca
Thomas <2875265-tclna@users.noreply.gitlab.com>
2022-11-05 15:50:42 +0100

    Simplify login and register pages with formik tag

--- Artisashop/ClientApp/src/pages/Login/index.tsx
+++ Artisashop/ClientApp/src/pages/Login/index.tsx
@@ -1,47 +1,39 @@
 import React from "react";
-import {useFormik} from "formik";
-import {useNavigate} from "react-router";
-import {Link} from "react-router-dom";
-import {useAuth} from "../../hooks/useAuth";
-import {Wrapper} from "./styles";
+import { Field, Form, Formik } from "formik";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import { Wrapper } from "./styles";
 
 const Login = () => {
   const auth = useAuth()
   const navigate = useNavigate()
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: (values => {
-        auth?.signin(values.email, values.password)
-          .then(res => res.user && navigate("/"))
-      }
-    )
-  })
 
   return (
     <Wrapper>
       <div className="register-card">
         <h1>Artisashop</h1>
         <h2>Connexion</h2>
-        <form onSubmit={formik.handleSubmit}>
-          <input className="credentials"
-                 name="email"
-                 type="email"
-                 placeholder="Email"
-                 value={formik.values.email}
-                 onChange={formik.handleChange}
-          />
-          <input className="credentials"
-                 name="password"
-                 type="password"
-                 placeholder="Mot de passe"
-                 value={formik.values.password}
-                 onChange={formik.handleChange}
-          />
-          <button type="submit" id="register-button" className="red-button">Se connecter</button>
-        </form>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          onSubmit={values => {
+            auth?.signin(values.email, values.password)
+              .then(res => res.user && navigate("/"))
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field className="credentials" name="email" type="email" placeholder="Email" />
+              <Field className="credentials" name="password" type="password" placeholder="Mot de passe" />
+              <button type="submit" id="register-button" className="red-button" disabled={isSubmitting}>
+                Se connecter
+              </button>
+            </Form>
+          )}
+        </Formik>
         <p>Pas encore inscrit? <Link to="/register">Rejoignez-nous</Link> !</p>
       </div>
     </Wrapper>
--- Artisashop/ClientApp/src/pages/Register/index.tsx
+++ Artisashop/ClientApp/src/pages/Register/index.tsx
@@ -1,83 +1,58 @@
 import React from "react";
-import {useNavigate} from "react-router";
-import {useFormik} from "formik";
-import {Link} from "react-router-dom";
-import {Wrapper} from "./styles";
-import {useAuth} from "../../hooks/useAuth";
-import {UserType} from "../../api";
+import { useNavigate } from "react-router";
+import { Formik, Field, Form } from "formik";
+import { Link } from "react-router-dom";
+import { Wrapper } from "./styles";
+import { useAuth } from "../../hooks/useAuth";
+import { UserType } from "../../api";
 
 const Register = () => {
   const auth = useAuth()
   const navigate = useNavigate()
-  const formik = useFormik({
-    initialValues: {
-      firstname: '',
-      lastname: '',
-      email: '',
-      password: '',
-      passwordConfirm: '',
-      role: UserType.NUMBER_0,
-    },
-    validate: (values => {
-      let error = {};
-
-      if (values.password !== values.passwordConfirm) {
-        error = "*Les mots de passe ne sont pas identiques";
-      }
-
-      return error;
-    }),
-    onSubmit: (values => {
-        auth?.signup(values)
-          .then(res => res.user && navigate("/"))
-      }
-    )
-  })
 
   return (
     <Wrapper>
       <div className="register-card">
         <h1>Artisashop</h1>
         <h2>Créer un compte <span id="account-type">client</span></h2>
-        <form onSubmit={formik.handleSubmit}>
-          <div id="register-name">
-            <input type="text"
-                   name="firstname"
-                   placeholder="Prénom"
-                   value={formik.values.firstname}
-                   onChange={formik.handleChange}
-            />
-            <input type="text"
-                   name="lastname"
-                   placeholder="Nom"
-                   value={formik.values.lastname}
-                   onChange={formik.handleChange}
-            />
-          </div>
-          <input className="credentials"
-                 name="email"
-                 type="email"
-                 placeholder="Email"
-                 value={formik.values.email}
+        <Formik
+          initialValues={{
+            firstname: '',
+            lastname: '',
+            email: '',
+            password: '',
+            passwordConfirm: '',
+            role: UserType.NUMBER_0,
+          }}
 
-                 onChange={formik.handleChange}
-          />
-          <input className="credentials"
-                 name="password"
-                 type="password"
-                 placeholder="Mot de passe"
-                 value={formik.values.password}
-                 onChange={formik.handleChange}
-          />
-          <input className="credentials" 
-                 name="passwordConfirm" 
-                 type="password" 
-                 placeholder="Confirmer mot de passe"
-                 value={formik.values.passwordConfirm}
-                 onChange={formik.handleChange}
-          />
-          <button type="submit" id="register-button" className="red-button">Créer un compte</button>
-        </form>
+          onSubmit={values => {
+            auth?.signup(values)
+              .then(res => res.user && navigate("/"))
+          }}
+
+          validate={values => {
+            const errors: { [key: string]: string } = {};
+            if (values.password !== values.passwordConfirm) {
+              errors.password = "*Les mots de passe ne sont pas identiques";
+            }
+            return errors;
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <div id="register-name">
+                <Field type="text" name="firstname" placeholder="Prénom" />
+                <Field type="text" name="lastname" placeholder="Nom" />
+              </div>
+              <Field className="credentials" name="email" type="email" placeholder="Email" />
+              <Field className="credentials" name="password" type="password" placeholder="Mot de passe" />
+              <Field className="credentials" name="passwordConfirm" type="password" placeholder="Confirmer mot de passe" />
+              <button type="submit" id="register-button" className="red-button" disabled={isSubmitting}>
+                Créer un compte
+              </button>
+            </Form>
+          )}
+        </Formik>
         <p>Déjà inscrit? <Link to="/login">Connectez-vous</Link> !</p>
       </div>
     </Wrapper>
