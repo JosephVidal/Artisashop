cdc156ecd9147aa2af8683291cef83d6480903fc
Thomas <2875265-tclna@users.noreply.gitlab.com>
2022-11-06 16:26:55 +0100

    Update link in TopMenu

--- Artisashop/ClientApp/src/components/TopMenu/index.tsx
+++ Artisashop/ClientApp/src/components/TopMenu/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Logo from "assets/LogoOvale.png";
 import { BaseText } from "globals/styles";
+import { Link } from "react-router-dom";
 import { Image } from "primereact/image";
 import { useTranslation } from "react-i18next";
 import { BsCart, BsChat, BsPerson } from "react-icons/bs";
 import { Quarter, TopMenuWrapper, MenuItem, RightIcons } from "./styles";
 
-interface Props {}
+interface Props { }
 //
 // const MenuItemCornerRight = styled(MenuItem)`
 //   position: absolute;
@@ -29,16 +30,16 @@ const TopMenu: React.FunctionComponent<Props> = () => {
           {t("menu.search")}
         </BaseText>
       </MenuItem>
-      <Image src={Logo} width="50px" />
+      <Link to="/"><Image src={Logo} width="50px" /></Link>
       <MenuItem>
         <BaseText onClick={() => console.log("Contact")}>
           {t("menu.contact")}
         </BaseText>
       </MenuItem>
       <RightIcons>
-        <BsCart size="100%" onClick={() => console.log("Cart")} />
-        <BsChat size="100%" onClick={() => console.log("Chat")} />
-        <BsPerson size="100%" onClick={() => console.log("Account")} />
+        <Link to="/mon-panier"><BsCart size="100%" /></Link>
+        <Link to="/chat"><BsChat size="100%" /></Link>
+        <Link to="/login"><BsPerson size="100%" /></Link>
       </RightIcons>
     </TopMenuWrapper>
   );
