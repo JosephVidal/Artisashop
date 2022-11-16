5ae7e210e6c7b2774eca3db1b98dab9d14272d15
Thomas <2875265-tclna@users.noreply.gitlab.com>
2022-11-16 19:19:20 +0100

    Update admin Create pages

--- Artisashop/ClientApp/src/pages/Admin/Resources/Users/Create.tsx
+++ Artisashop/ClientApp/src/pages/Admin/Resources/Users/Create.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import { Create, SimpleForm, TextInput } from 'react-admin';
+import { Create, SelectField, SimpleForm, TextInput } from 'react-admin';
 
-const CreateUser = () => (
-  <Create>
-    <SimpleForm>
-      <TextInput source="firstname" />
-      <TextInput source="lastname" />
-    </SimpleForm>
-  </Create>
-);
+const choices=[
+  { id: 'USER', name: 'User' },
+  { id: 'ADMIN', name: 'Admin' },
+];
+
+interface TransformProps {
+  email: string;
+  firstname: string;
+  lastname: string;
+  role: string;
+}
+
+const CreateUser = () => {
+  const transform = (data : TransformProps) => ({
+    ...data,
+    userName: data.email,
+    normalizedUserName: data.email.toUpperCase(),
+  })
+  
+  return (
+    <Create transform={transform}>
+      <SimpleForm>
+        <TextInput source="email" />
+        <TextInput source="firstname" />
+        <TextInput source="lastname" />
+        <SelectField source="role" choices={choices} />
+      </SimpleForm>
+    </Create>
+  );
+};
 
 export default CreateUser;
