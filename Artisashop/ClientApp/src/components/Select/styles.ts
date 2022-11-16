d96103b2ae7e6dd2f14f6b5fca8b90a8a8c91de0
Héléna Ganeman-Valot <helena.ganeman-valot@epitech.eu>
2022-11-16 14:54:00 +0100

    [Frontend][Basket]: Push before merge

--- Artisashop/ClientApp/src/components/Select/styles.ts
+++ Artisashop/ClientApp/src/components/Select/styles.ts
@@ -10,5 +10,15 @@ export const SelectWrapper = styled.div<SelectWrapperProps>`
 
   & .p-dropdown {
     display: flex;
+    transition: background-color 0.2s, color 0.2s, border-color 0.2s,
+    box-shadow 0.2s;
+    border-radius: 5px;
+  }
+  .p-dropdown .p-dropdown-trigger {
+    background: transparent;
+    color: rgba(255, 255, 255, 0.6);
+    width: 2.357rem;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
   }
 `;
