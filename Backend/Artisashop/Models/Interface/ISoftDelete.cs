2a0e9c383700a21eed3c89bac3f08ae20bfdfb51
Thomas <2875265-tclna@users.noreply.gitlab.com>
2022-11-21 23:11:26 +0100

    Add ISoftDelete

---
+++ Backend/Artisashop/Models/Interface/ISoftDelete.cs
@@ -0,0 +1,27 @@
+﻿namespace Artisashop.Models.Interface;
+
+public interface ISoftDelete
+{
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
+    public bool Delete()
+    {
+        if (IsDeleted) return false;
+
+        IsDeleted = true;
+        DeletedAt = DateTime.Now;
+
+        return true;
+    }
+
+    public bool Restore()
+    {
+        if (!IsDeleted) return false;
+
+        IsDeleted = false;
+        DeletedAt = null;
+
+        return true;
+    }
+}
