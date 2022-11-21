3cabd3d02476651c797ce8dd7b2e6f74b7883df0
Thomas <2875265-tclna@users.noreply.gitlab.com>
2022-11-21 23:11:54 +0100

    Add PageSelection view model

---
+++ Backend/Artisashop/Models/ViewModel/PageSelection.cs
@@ -0,0 +1,10 @@
+﻿namespace Artisashop.Models.ViewModel;
+
+/// <summary>
+/// Used to only take a certain amount of items from a list.
+/// </summary>
+public class PageSelection
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+}
