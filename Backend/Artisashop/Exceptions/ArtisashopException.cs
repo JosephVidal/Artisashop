1e277f5e1ca03cf3529810af4907bb83702b6837
Thomas <2875265-tclna@users.noreply.gitlab.com>
2022-11-21 21:16:51 +0100

    Create ArtisashopException

---
+++ Backend/Artisashop/Exceptions/ArtisashopException.cs
@@ -0,0 +1,31 @@
+﻿namespace Artisashop.Exceptions;
+
+using System;
+using System.Runtime.Serialization;
+
+/// <summary>
+/// Exception de base Artisashop
+/// </summary>
+[Serializable]
+public class ArtisashopException : Exception
+{
+    public ArtisashopException()
+    {
+    }
+
+    public ArtisashopException(string message)
+        : base(message)
+    {
+    }
+
+    public ArtisashopException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    // Without this constructor, deserialization will fail
+    protected ArtisashopException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+    }
+}
