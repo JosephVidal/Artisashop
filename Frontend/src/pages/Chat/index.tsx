3f99ddef9fbea196a434acee89e06c1cc2cd0d2b
Thomas <2875265-tclna@users.noreply.gitlab.com>
2022-12-29 00:19:33 +0100

    Update message overflow

--- Frontend/src/pages/Chat/index.tsx
+++ Frontend/src/pages/Chat/index.tsx
@@ -6,7 +6,7 @@ const ContactList = () => (
 
 const MessageList = () => (
   <div className="flex flex-col gap-3">
-    {Array.from({ length: 10 }).map((_, index) => (
+    {Array.from({ length: 25 }).map((_, index) => (
       <div key={index} className="flex gap-5">
         <img src="https://picsum.photos/200" alt="avatar" className="rounded-full w-7 h-7" />
         <div className="flex flex-col">
@@ -24,13 +24,13 @@ const MessageForm = () => (
 
 const ChatPage = () => (
   <div className="text-black px-6 pb-5">
-    <h1>Chat Page</h1>
+    <h1>Messagerie</h1>
     <div className="flex flex-col md:flex-row justify-between gap-5">
       <div className="md:w-1/4 p-5 border rounded-3xl border-solid border-black">
         <ContactList />
       </div>
       <div className="flex-1 flex flex-col gap-5 p-5 border rounded-3xl border-solid border-black">
-        <div>
+        <div className="overflow-auto overscroll-contain max-h-96">
           <MessageList />
         </div>
         <div>
