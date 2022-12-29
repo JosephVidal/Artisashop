2f9215eca576191efe7f5b2c47488b47e2fd65b5
Thomas <2875265-tclna@users.noreply.gitlab.com>
2022-12-29 01:37:35 +0100

    WIP

--- Frontend/src/pages/Chat/index.tsx
+++ Frontend/src/pages/Chat/index.tsx
@@ -1,41 +1,95 @@
+import { PrimitiveAtom, atom, useAtom } from 'jotai';
+import { splitAtom } from 'jotai/utils';
 import React from 'react';
 
-const Avatar = ({
-  src = 'https://picsum.photos/200',
-}) => (
-  <img src={src} alt="avatar" className="rounded-full w-8 h-8" />
-);
+interface ContactListItemProps {
+  name: string;
+  lastMessage: string;
+  avatarSrc?: string;
+  selected: boolean;
+}
+
+const initialContactList: ContactListItemProps[] = Array.from({ length: 10 }).map((_, index) => ({
+  name: `Name ${index}`,
+  lastMessage: `Last message ${index}`,
+  avatarSrc: 'https://picsum.photos/200',
+  selected: false,
+}));
+
+const contactListAtom = atom<ContactListItemProps[]>(initialContactList);
+const contactListAtomAtom = splitAtom(contactListAtom);
+const selectedIndexAtom = atom(0);
+
+const setSelectedIndex = (index: number) => {
+  const [contactList] = useAtom(contactListAtom);
+  const [selectedIndex, iSetSelectedIndex] = useAtom(selectedIndexAtom);
+
+  contactList[selectedIndex].selected = false;
+  iSetSelectedIndex(index);
+};
 
 const ContactListItem = ({
-  name = 'Name',
-  lastMessage = 'Last message',
-  avatarSrc = 'https://picsum.photos/200',
+  contactListItemAtom,
+}: {
+  contactListItemAtom: PrimitiveAtom<ContactListItemProps>,
 }) => {
-  const [selected, setSelected] = React.useState(false);
+  const [contactListItem, setContactListItem] = useAtom(contactListItemAtom);
+  const { name, lastMessage, avatarSrc, selected } = contactListItem;
 
   const handleClick = () => {
-    setSelected(!selected);
+    setContactListItem((prev) => ({
+      ...prev,
+      selected: !prev.selected,
+    }));
   };
 
-    return (
-      <div onClick={handleClick} className="bg-slate-800 rounded-xl p-2 text-white">
-        <div className="flex gap-2 items-center">
-          <Avatar src={avatarSrc} />
-          <div className="flex flex-col">
-            <div className="text-sm">{name}</div>
-            <div className="text-xs">{lastMessage}</div>
-          </div>
+  return (
+    <div onClick={handleClick} className="select-none">
+      <div className={`
+          overflow-hidden relative max-w-sm mx-auto 
+        ${selected ? "bg-[#141C26]" : "bg-[#F9ECE2]"} shadow-lg ring-1 ring-black/5 rounded-xl flex items-center gap-6 
+         dark:highlight-white/5`}>
+        <img className="absolute -left-6 w-24 h-24 rounded-full shadow-lg" src={avatarSrc} alt={name} />
+        <div className="flex flex-col py-5 pl-24">
+          <strong className="text-slate-900 text-base font-medium dark:text-slate-200">{name}</strong>
+          <span className="text-slate-500 text-sm font-medium dark:text-slate-400">{lastMessage}</span>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-const ContactList = () => (
-  <div>
-    {/* <div>ContactList</div> */}
-    <div className="flex flex-col gap-1">
-      {Array.from({ length: 10 }).map((_, index) => (
-        <ContactListItem key={index} />
+const ContactList = () => {
+  const [contactListAtoms] = useAtom(contactListAtomAtom);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2">
+        {contactListAtoms.map((item, index) => (
+          <ContactListItem contactListItemAtom={item} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const ChatMessageBubble = () => (
+  <div className="flex gap-5">
+    <div className="flex flex-col">
+      <div className="text-xs">Last message</div>
+    </div>
+  </div>
+)
+
+const ChatMessageBubbleGroup = () => (
+  <div className="flex flex-row">
+    <div>
+      <img src="https://picsum.photos/200" alt="avatar" className="rounded-full w-8 h-8" />
+      <div className="text-sm">Name</div>
+    </div>
+    <div className="flex flex-col gap-3">
+      {Array.from({ length: 3 }).map((_, index) => (
+        <ChatMessageBubble key={index} />
       ))}
     </div>
   </div>
@@ -44,13 +98,7 @@ const ContactList = () => (
 const MessageList = () => (
   <div className="flex flex-col gap-3">
     {Array.from({ length: 25 }).map((_, index) => (
-      <div key={index} className="flex gap-5">
-        <Avatar />
-        <div className="flex flex-col">
-          <div className="text-sm">Name</div>
-          <div className="text-xs">Last message</div>
-        </div>
-      </div>
+      <ChatMessageBubbleGroup key={index} />
     ))}
   </div>
 );
@@ -63,11 +111,15 @@ const ChatPage = () => (
   <div className="text-black px-6 pb-5">
     <h1>Messagerie</h1>
     <div className="flex flex-col md:flex-row justify-between gap-5">
-      <div className="md:w-1/4 p-5 border rounded-3xl border-solid border-black">
-        <ContactList />
+      <div className="md:w-1/3 md:max-h-96 flex flex-col gap-5 p-3 border rounded-3xl border-solid border-black text-white">
+        {/* <div className="md:w-1/4 p-3 border rounded-3xl border-solid border-black"> */}
+        {/* <div className="text-xl font-bold">Contacts</div> */}
+        <div className="overflow-auto overscroll-contain px-1">
+          <ContactList />
+        </div>
       </div>
-      <div className="flex-1 flex flex-col gap-5 p-5 border rounded-3xl border-solid border-black">
-        <div className="overflow-auto overscroll-contain max-h-96">
+      <div className="md:max-h-96 flex-1 flex flex-col gap-5 p-5 border rounded-3xl border-solid border-black">
+        <div className="overflow-auto overscroll-contain">
           <MessageList />
         </div>
         <div>
