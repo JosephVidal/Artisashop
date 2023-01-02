ebb6964a53331e56e5e0fdb369d291c6f6032a06
Thomas <2875265-tclna@users.noreply.gitlab.com>
2023-01-02 21:54:34 +0100

    Update admin page

--- Frontend/src/pages/Admin/index.tsx
+++ Frontend/src/pages/Admin/index.tsx
@@ -1,18 +1,19 @@
 import * as React from "react";
-import {Admin, EditGuesser, ListGuesser, Resource, ShowGuesser} from "react-admin";
+import { Admin, EditGuesser, ListGuesser, Resource, ShowGuesser, fetchUtils } from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
-import {REACT_APP_API_URL} from "conf";
+import { REACT_APP_API_URL } from "conf";
 
 import useFormattedDocumentTitle from "hooks/useFormattedDocumentTitle";
-import ProductList from "./Resources/Products/List";
 
+import ProductList from "./Resources/Products/List";
 import UserCreate from "./Resources/Users/Create";
+import fetchJson from "./fetchJson";
 
 const AdminDashboard = () => {
   useFormattedDocumentTitle("Dashboard Admin");
-  
+
   return (
-    <Admin basename="/admin" dataProvider={simpleRestProvider(`${REACT_APP_API_URL}/api/admin`)}>
+    <Admin basename="/admin" dataProvider={simpleRestProvider(`${REACT_APP_API_URL}/api/admin`, fetchJson)}>
       <Resource name="product" list={ProductList} edit={EditGuesser} show={ShowGuesser} />
       <Resource name="complaint" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
       <Resource name="user" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} create={UserCreate} />
